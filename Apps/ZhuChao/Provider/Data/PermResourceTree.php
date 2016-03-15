ef7235d45165777d24c7e28af58256be622af99a
Changwang <chenyongwang1104@163.com>
2016-03-15 18:07:01 +0800

    修复一处错误

--- Apps/ZhuChao/Provider/Data/PermResourceTree.php
+++ Apps/ZhuChao/Provider/Data/PermResourceTree.php
@@ -6,6 +6,7 @@
  * @copyright  Copyright (c) 2010-2011 Cntysoft Technologies China Inc. <http://www.cntysoft.com>
  * @license    http://www.cntysoft.com/license/new-bsd     New BSD License
  */
+use App\ZhuChao\Provider\Constant;
 return array(
    'text'        => '供应商管理',
    'internalKey' => 'Provider',
