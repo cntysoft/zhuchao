dfdebe1c9d6e4eccc191b46f018d10e5e50615fa
Changwang <chenyongwang1104@163.com>
2016-03-17 17:30:56 +0800

    添加用户中心上传接口

--- TagLibrary/Label/帮助中心/帮助中心/Script.php
+++ TagLibrary/Label/帮助中心/帮助中心/Script.php
@@ -1,70 +1,74 @@
-<?php
-
+<?php 
 namespace TagLibrary\Label\HelpCenter;
-
 use Cntysoft\Framework\Qs\Engine\Tag\AbstractLabelScript;
 use App\Site\Category\Constant as CATEGORY_CONST;
 use App\Site\Content\Constant as CONTENT_CONST;
+class HelpCenter extends AbstractLabelScript
+{
+   /**
+    * 检查节点是否存在
+    * @param string $identifier
+    * @return boolean
+    */
+   public function checkNodeIdentifier($identifier)
+   {
+      return $this->appCaller->call(
+                      CATEGORY_CONST::MODULE_NAME, CATEGORY_CONST::APP_NAME, CATEGORY_CONST::APP_API_STRUCTURE, 'checkNodeIdentifier', array($identifier));
+   }
 
-class HelpCenter extends AbstractLabelScript {
+   /**
+    * 获取节点信息
+    * @param string $identifier
+    * @return 
+    */
+   public function getNodeInfoByIdentifier($identifier)
+   {
+      $this->checkNodeIdentifier($identifier);
+      $nodeInfo = $this->appCaller->call(CATEGORY_CONST::MODULE_NAME, CATEGORY_CONST::APP_NAME, CATEGORY_CONST::APP_API_STRUCTURE, 'getNodeByIdentifier', array($identifier));
+      return $nodeInfo;
+   }
 
-	/**
-	 * 检查节点是否存在
-	 * @param string $identifier
-	 * @return boolean
-	 */
-	public function checkNodeIdentifier($identifier) {
-		return $this->appCaller->call(
-							 CATEGORY_CONST::MODULE_NAME, CATEGORY_CONST::APP_NAME, CATEGORY_CONST::APP_API_STRUCTURE, 'checkNodeIdentifier', array($identifier));
-	}
+   /**
+    * 获取节点的字节点信息
+    * @param string $identilier
+    * @return 
+    */
+   public function getSubNodesByIdentifier($identifier)
+   {
+      $nodeInfo = $this->getNodeInfoByIdentifier($identifier);
+      $nodeId = $nodeInfo->getId();
+      $childNodes = $this->appCaller->call(CATEGORY_CONST::MODULE_NAME, CATEGORY_CONST::APP_NAME, CATEGORY_CONST::APP_API_STRUCTURE, 'getSubNodes', array($nodeId));
+      return $childNodes;
+   }
 
-	/**
-	 * 获取节点信息
-	 * @param string $identifier
-	 * @return 
-	 */
-	public function getNodeInfoByIdentifier($identifier) {
-		$this->checkNodeIdentifier($identifier);
-		$nodeInfo = $this->appCaller->call(CATEGORY_CONST::MODULE_NAME, CATEGORY_CONST::APP_NAME, CATEGORY_CONST::APP_API_STRUCTURE, 'getNodeByIdentifier', array($identifier));
-		return $nodeInfo;
-	}
+   public function getInfoListByNodeAndStatus($nodeId)
+   {
+      $limit = $this->invokeParams['outputNum'];
+      $page = $this->getRouteInfo()['pageid'];
+      $offset = ((int) $page - 1) * $limit;
+      $generalInfo = $this->appCaller->call(
+              CONTENT_CONST::MODULE_NAME, CONTENT_CONST::APP_NAME, CONTENT_CONST::APP_API_INFO_LIST, 'getInfoListByNodeAndStatus', array($nodeId, 1, 3, true, 'id DESC', $offset, $limit));
+      return $generalInfo;
+   }
 
-	/**
-	 * 获取节点的字节点信息
-	 * @param string $identilier
-	 * @return 
-	 */
-	public function getSubNodesByIdentifier($identifier) {
-		$nodeInfo = $this->getNodeInfoByIdentifier($identifier);
-		$nodeId = $nodeInfo->getId();
-		$childNodes = $this->appCaller->call(CATEGORY_CONST::MODULE_NAME, CATEGORY_CONST::APP_NAME, CATEGORY_CONST::APP_API_STRUCTURE, 'getSubNodes', array($nodeId));
-		return $childNodes;
-	}
+   public function getInfoListByNodeAndStatusNotPage($nodeId)
+   {
+      $limit = $this->invokeParams['outputNum'];
+      $generalInfo = $this->appCaller->call(
+              CONTENT_CONST::MODULE_NAME, CONTENT_CONST::APP_NAME, CONTENT_CONST::APP_API_INFO_LIST, 'getInfoListByNodeAndStatus', array($nodeId, 1, 3, false, 'hits DESC', 0, $limit));
+      return $generalInfo;
+   }
 
-	public function getInfoListByNodeAndStatus($nodeId) {
-		$limit = $this->invokeParams['outputNum'];
-		$page = $this->getRouteInfo()['pageid'];
-		$offset = ((int) $page - 1) * $limit;
-		$generalInfo = $this->appCaller->call(
-				  CONTENT_CONST::MODULE_NAME, CONTENT_CONST::APP_NAME, CONTENT_CONST::APP_API_INFO_LIST, 'getInfoListByNodeAndStatus', array($nodeId, 1, 3, true, 'id DESC', $offset, $limit));
-		return $generalInfo;
-	}
+   public function getGInfo($id)
+   {
+      return $this->appCaller->call(
+                      CONTENT_CONST::MODULE_NAME, CONTENT_CONST::APP_NAME, CONTENT_CONST::APP_API_MANAGER, 'getGInfo', array($id));
+   }
 
-	public function getInfoListByNodeAndStatusNotPage($nodeId) {
-		$limit = $this->invokeParams['outputNum'];
-		$generalInfo = $this->appCaller->call(
-				  CONTENT_CONST::MODULE_NAME, CONTENT_CONST::APP_NAME, CONTENT_CONST::APP_API_INFO_LIST, 'getInfoListByNodeAndStatus', array($nodeId, 1, 3, false, 'hits DESC', 0, $limit));
-		return $generalInfo;
-	}
-
-	public function getGInfo($id) {
-		return $this->appCaller->call(
-							 CONTENT_CONST::MODULE_NAME, CONTENT_CONST::APP_NAME, CONTENT_CONST::APP_API_MANAGER, 'getGInfo', array($id));
-	}
-
-	public function getPrevAndNextItem($nid, $id) {
-		return $this->appCaller->call(
-							 CONTENT_CONST::MODULE_NAME, CONTENT_CONST::APP_NAME, CONTENT_CONST::APP_API_INFO_LIST, 'getPrevAndNextItem', array($nid, $id));
-	}
+   public function getPrevAndNextItem($nid, $id)
+   {
+      return $this->appCaller->call(
+                      CONTENT_CONST::MODULE_NAME, CONTENT_CONST::APP_NAME, CONTENT_CONST::APP_API_INFO_LIST, 'getPrevAndNextItem', array($nid, $id));
+   }
 
 }
