9de282447cce40b678372f2b2c7466538a4279f4
Arvin <cntyfeng@163.com>
2016-03-21 19:24:03 +0800

    实现采购商的用户中心左侧

---
+++ TagLibrary/Label/供应商/用户中心左侧/Definition.php
@@ -0,0 +1,11 @@
+<?php
+return array(
+   'id'          => '用户中心左侧',
+   'category'    => '供应商',
+   'static'      => false,
+   'namespace'   => 'Provider',
+   'class'       => 'UserLeft',
+   'description' => '获取用户中心左侧的内容',
+   'attributes'  => array(
+   )
+);
