0b8b60c86e1a2cdb78b2c8cdc8c3ae0a50124bb1
wql <wql1211608804@163.com>
2016-03-28 13:34:08 +0800

    修改手机版分类列表展示

---
+++ TagLibrary/Ds/商品数据源/商品分类信息/Definition.php
@@ -0,0 +1,10 @@
+<?php
+return array(
+   'id'          => '商品分类信息',
+   'category'    => '商品数据源',
+   'class'       => 'GoodsCate',
+   'namespace'   => 'Goods',
+   'description' => '商品模型数据源标签',
+   'attributes'  => array(
+   )
+);
---
+++ TagLibrary/Ds/商品数据源/商品分类信息/Script.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * Cntysoft Web Platform 中国领先的web平台
+ *
+ * @category   Cntysoft
+ * @author     SOFTBOY <cntysoft@163.com>
+ * @copyright  Copyright (c) 2010-2011 Cntysoft Technologies China Inc. <http://www.cntysoft.com>
+ * @license    http://www.cntysoft.com/license/new-bsd     New BSD License
+ */
+namespace TagLibrary\Ds\Goods;
+use Cntysoft\Framework\Qs\Engine\Tag\AbstractDsScript;
+use App\ZhuChao\CategoryMgr\Constant as CATE_CONST;
+class GoodsCate extends AbstractDsScript
+{
+   /**
+    * @inheritdoc
+    */
+   public function load()
+   {
+      $number = $this->getRouteInfo()['categoryId'];
+      $nodetree = $this->appCaller->call(CATE_CONST::MODULE_NAME, CATE_CONST::APP_NAME, CATE_CONST::APP_API_MGR, 'getNodeTree', array());
+      $nodeInfo = $nodetree->getParent($number, true);
+      return array(
+         'name' => $nodeInfo->getName()
+      );
+   }
+
+}
