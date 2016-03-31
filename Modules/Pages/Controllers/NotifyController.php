c5464d01a3501fdea5220a0ef48e2e43da1c105f
ZhiHui <liuyan2526@qq.com>
2016-03-31 10:09:23 +0800

    正佳畅言评论

---
+++ Modules/Pages/Controllers/NotifyController.php
@@ -0,0 +1,72 @@
+<?php
+/**
+ * Cntysoft Cloud Software Team
+ * 
+ * @author ZhiHui <liuyan2526@qq.com>
+ * @copyright Copyright (c) 2010-2011 Cntysoft Technologies China Inc. <http://www.cntysoft.com>
+ * @license http://www.cntysoft.com/license/new-bsd     New BSD License
+ */
+use Cntysoft\Phalcon\Mvc\AbstractController;
+use App\ZhuChao\Buyer\Constant as BUYER_CONST;
+use Cntysoft\Kernel\ConfigProxy;
+/**
+ * 系统的回接口
+ */
+class NotifyController extends AbstractController
+{
+	public function changyanGetAclAction()
+	{
+		try {
+			$curUser = $this->getAppCaller()->call(
+					  BUYER_CONST::MODULE_NAME, BUYER_CONST::APP_NAME, BUYER_CONST::APP_API_BUYER_ACL, 'getCurUser');
+			if ($curUser) {
+				$detail = $curUser->getProfile();
+				$data = array(
+					'is_login'		 => 1,
+					'user_id'		 => $curUser->getId(),
+					'nickname'		 => $curUser->getName() ? $curUser->getName() : $curUser->getPhone(),
+					'img_url'		 => \Cntysoft\Kernel\get_image_cdn_url($detail->getAvatar(), 100, 100),
+					'profile_url'	 => 'http://' . \Cntysoft\RT_SYS_SITE_NAME,
+					'sign'			 => $this->sign(\Cntysoft\Kernel\get_image_cdn_url($detail->getAvatar(), 100, 100), $curUser->getName() ? $curUser->getName() : $curUser->getPhone(), 'http://' . \Cntysoft\RT_SYS_SITE_NAME, $curUser->getId())
+				);
+				echo $_GET['callback'].'('.json_encode($data).')';
+			}
+		} catch (\Exception $exc) {
+			$data = array(
+				'is_login' => 0
+			);
+			echo $_GET['callback'].'('.json_encode($data).')';
+		}
+	}
+	/**
+	 * 生成changyan签名
+	 * @param type $imgUrl
+	 * @param type $nickname
+	 * @param type $profileUrl
+	 * @param type $isvUserId
+	 * @return type
+	 */
+	private function sign($imgUrl, $nickname, $profileUrl, $isvUserId)
+	{
+		$mate = $this->getChangyanConfig();
+		$toSign = "img_url=" . $imgUrl . "&nickname=" . $nickname . "&profile_url=" . $profileUrl . "&user_id=" . $isvUserId;
+		$signature = base64_encode(hash_hmac("sha1", $toSign, $mate['appkey'], true));
+		return $signature;
+	}
+	/**
+	 * 获取changyanID和secret
+	 * @return array
+	 */
+	private function getChangyanConfig()
+	{
+		$netCfg = ConfigProxy::getFrameworkConfig('Net');
+		if (!isset($netCfg['changYan']) || !isset($netCfg['changYan']['appid']) || !isset($netCfg['changYan']['appkey'])) {
+			$errorType = ErrorType::getInstance();
+			Kernel\throw_exception(new Exception(
+					  $errorType->msg('E_SDK_CONFIG_NOT_EXIST'), $errorType->code('E_SDK_CONFIG_NOT_EXIST')
+			));
+		}
+		return $netCfg->changYan;
+	}
+
+}
--- TagLibrary/Label/帮助中心/帮助中心/Script.php
+++ TagLibrary/Label/帮助中心/帮助中心/Script.php
@@ -11,52 +11,54 @@ namespace TagLibrary\Label\HelpCenter;
 use Cntysoft\Framework\Qs\Engine\Tag\AbstractLabelScript;
 use App\Site\Category\Constant as CATEGORY_CONST;
 use App\Site\Content\Constant as CONTENT_CONST;
+use Cntysoft\Kernel\ConfigProxy;
 class HelpCenter extends AbstractLabelScript
 {
-   /**
-    * 路由信息
-    *
-    * @var null
-    */
-   protected $routes = null;
+	/**
+	 * 路由信息
+	 *
+	 * @var null
+	 */
+	protected $routes = null;
 
-   /**
-    * 检查节点是否存在
-    * @param string $identifier
-    * @return boolean
-    */
-   public function checkNodeIdentifier($identifier)
-   {
-      return $this->appCaller->call(
-                      CATEGORY_CONST::MODULE_NAME, CATEGORY_CONST::APP_NAME, CATEGORY_CONST::APP_API_STRUCTURE, 'checkNodeIdentifier', array($identifier));
-   }
+	/**
+	 * 检查节点是否存在
+	 * @param string $identifier
+	 * @return boolean
+	 */
+	public function checkNodeIdentifier($identifier)
+	{
+		return $this->appCaller->call(
+							 CATEGORY_CONST::MODULE_NAME, CATEGORY_CONST::APP_NAME, CATEGORY_CONST::APP_API_STRUCTURE, 'checkNodeIdentifier', array($identifier));
+	}
 
-   /**
-    * 获取节点信息
-    * @param string $identifier
-    * @return 
-    */
-   public function getNodeInfoByIdentifier($identifier)
-   {
-      $this->checkNodeIdentifier($identifier);
-      $nodeInfo = $this->appCaller->call(
-              CATEGORY_CONST::MODULE_NAME, CATEGORY_CONST::APP_NAME, CATEGORY_CONST::APP_API_STRUCTURE, 'getNodeByIdentifier', array($identifier));
-      return $nodeInfo;
-   }
+	/**
+	 * 获取节点信息
+	 * @param string $identifier
+	 * @return 
+	 */
+	public function getNodeInfoByIdentifier($identifier)
+	{
+		$this->checkNodeIdentifier($identifier);
+		$nodeInfo = $this->appCaller->call(
+				  CATEGORY_CONST::MODULE_NAME, CATEGORY_CONST::APP_NAME, CATEGORY_CONST::APP_API_STRUCTURE, 'getNodeByIdentifier', array($identifier));
+		return $nodeInfo;
+	}
 
-   /**
-    * 获取节点的字节点信息
-    * @param string $identilier
-    * @return 
-    */
-   public function getSubNodesByIdentifier($identifier)
-   {
-      $nodeInfo = $this->getNodeInfoByIdentifier($identifier);
-      $nodeId = $nodeInfo->getId();
-      $childNodes = $this->appCaller->call(
-              CATEGORY_CONST::MODULE_NAME, CATEGORY_CONST::APP_NAME, CATEGORY_CONST::APP_API_STRUCTURE, 'getSubNodes', array($nodeId));
-      return $childNodes;
-   }
+	/**
+	 * 获取节点的字节点信息
+	 * @param string $identilier
+	 * @return 
+	 */
+	public function getSubNodesByIdentifier($identifier)
+	{
+		$nodeInfo = $this->getNodeInfoByIdentifier($identifier);
+		$nodeId = $nodeInfo->getId();
+		$childNodes = $this->appCaller->call(
+				  CATEGORY_CONST::MODULE_NAME, CATEGORY_CONST::APP_NAME, CATEGORY_CONST::APP_API_STRUCTURE, 'getSubNodes', array($nodeId));
+		return $childNodes;
+	}
+
 	/**
 	 * 获取指定信息的详细内容
 	 *
@@ -68,72 +70,103 @@ class HelpCenter extends AbstractLabelScript
 		return $this->appCaller->call(CONTENT_CONST::MODULE_NAME, CONTENT_CONST::APP_NAME, CONTENT_CONST::APP_API_MANAGER, 'read', array($itemId));
 	}
 
-   /**
-    * 获取文章列表（带分页）
-    * 
-    * @param int $nodeId
-    * @return type
-    */
-   public function getInfoListByNodeAndStatus($nodeId)
-   {
-      $limit = $this->invokeParams['outputNum'];
-      $page = $this->getRouteInfo()['pageid'];
-      $offset = ((int) $page - 1) * $limit;
-      $generalInfo = $this->appCaller->call(
-              CONTENT_CONST::MODULE_NAME, CONTENT_CONST::APP_NAME, CONTENT_CONST::APP_API_INFO_LIST, 'getInfoListByNodeAndStatus', array($nodeId, 1, 3, true, 'id DESC', $offset, $limit));
-      return $generalInfo;
-   }
+	/**
+	 * 获取文章列表（带分页）
+	 * 
+	 * @param int $nodeId
+	 * @return type
+	 */
+	public function getInfoListByNodeAndStatus($nodeId)
+	{
+		$limit = $this->invokeParams['outputNum'];
+		$page = $this->getRouteInfo()['pageid'];
+		$offset = ((int) $page - 1) * $limit;
+		$generalInfo = $this->appCaller->call(
+				  CONTENT_CONST::MODULE_NAME, CONTENT_CONST::APP_NAME, CONTENT_CONST::APP_API_INFO_LIST, 'getInfoListByNodeAndStatus', array($nodeId, 1, 3, true, 'id DESC', $offset, $limit));
+		return $generalInfo;
+	}
 
-   /**
-    * 获取文章列表（不带分页）
-    * @param int $nodeId
-    * @return type
-    */
-   public function getInfoListByNodeAndStatusNotPage($nodeId)
-   {
-      $limit = $this->invokeParams['outputNum'];
-      $generalInfo = $this->appCaller->call(
-              CONTENT_CONST::MODULE_NAME, CONTENT_CONST::APP_NAME, CONTENT_CONST::APP_API_INFO_LIST, 'getInfoListByNodeAndStatus', array($nodeId, 1, 3, false, 'hits DESC', 0, $limit));
-      return $generalInfo;
-   }
+	/**
+	 * 获取文章列表（不带分页）
+	 * @param int $nodeId
+	 * @return type
+	 */
+	public function getInfoListByNodeAndStatusNotPage($nodeId)
+	{
+		$limit = $this->invokeParams['outputNum'];
+		$generalInfo = $this->appCaller->call(
+				  CONTENT_CONST::MODULE_NAME, CONTENT_CONST::APP_NAME, CONTENT_CONST::APP_API_INFO_LIST, 'getInfoListByNodeAndStatus', array($nodeId, 1, 3, false, 'hits DESC', 0, $limit));
+		return $generalInfo;
+	}
 
-   /**
-    * 获取文章内容
-    * @param int $id
-    * @return type
-    */
-   public function getGInfo($id)
-   {
-      return $this->appCaller->call(
-                      CONTENT_CONST::MODULE_NAME, CONTENT_CONST::APP_NAME, CONTENT_CONST::APP_API_MANAGER, 'getGInfo', array($id));
-   }
+	/**
+	 * 获取文章内容
+	 * @param int $id
+	 * @return type
+	 */
+	public function getGInfo($id)
+	{
+		return $this->appCaller->call(
+							 CONTENT_CONST::MODULE_NAME, CONTENT_CONST::APP_NAME, CONTENT_CONST::APP_API_MANAGER, 'getGInfo', array($id));
+	}
 
-   /**
-    * 获取上一篇下一篇文章
-    * @param int $nid
-    * @param int $id
-    * @return type
-    */
-   public function getPrevAndNextItem($nid, $id)
-   {
-      return $this->appCaller->call(
-                      CONTENT_CONST::MODULE_NAME, CONTENT_CONST::APP_NAME, CONTENT_CONST::APP_API_INFO_LIST, 'getPrevAndNextItem', array($nid, $id));
-   }
+	/**
+	 * 获取上一篇下一篇文章
+	 * @param int $nid
+	 * @param int $id
+	 * @return type
+	 */
+	public function getPrevAndNextItem($nid, $id)
+	{
+		return $this->appCaller->call(
+							 CONTENT_CONST::MODULE_NAME, CONTENT_CONST::APP_NAME, CONTENT_CONST::APP_API_INFO_LIST, 'getPrevAndNextItem', array($nid, $id));
+	}
 
-   public function getRouteNodeIdentifier()
-   {
-      return $this->getRoute()['nodeIdentifier'];
-   }
+	public function getRouteNodeIdentifier()
+	{
+		return $this->getRoute()['nodeIdentifier'];
+	}
 
-   /**
-    * 获取路由信息
-    */
-   public function getRoute()
-   {
-      if (null == $this->routes) {
-         $this->routes = $this->getRouteInfo();
-      }
-      return $this->routes;
-   }
+	/**
+	 * 获取路由信息
+	 */
+	public function getRoute()
+	{
+		if (null == $this->routes) {
+			$this->routes = $this->getRouteInfo();
+		}
+		return $this->routes;
+	}
+
+	public function openCommentPage()
+	{
+		$mate = $this->getChangyanConfig();
+		$router = $this->getRoute();
+		return '<div id="SOHUCS" sid="' . $router['articleId'] . '"></div>
+<script charset="utf-8" type="text/javascript" src="http://changyan.sohu.com/upload/changyan.js" ></script>
+<script type="text/javascript">
+    window.changyan.api.config({
+        appid: "' . $mate['appid'] . '",
+        conf: "' . $mate['appkey'] . '"
+    });
+</script>        
+';
+	}
+
+	/**
+	 * 获取changyanID和secret
+	 * @return array
+	 */
+	private function getChangyanConfig()
+	{
+		$netCfg = ConfigProxy::getFrameworkConfig('Net');
+		if (!isset($netCfg['changYan']) || !isset($netCfg['changYan']['appid']) || !isset($netCfg['changYan']['appkey'])) {
+			$errorType = ErrorType::getInstance();
+			Kernel\throw_exception(new Exception(
+					  $errorType->msg('E_SDK_CONFIG_NOT_EXIST'), $errorType->code('E_SDK_CONFIG_NOT_EXIST')
+			));
+		}
+		return $netCfg->changYan;
+	}
 
 }
