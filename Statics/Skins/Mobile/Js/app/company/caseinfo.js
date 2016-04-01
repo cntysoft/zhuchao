9a3b198628432fce7f2d886a918184e8ae61b770
wql <wql1211608804@163.com>
2016-04-01 22:20:47 +0800

    手机版案例详情

---
+++ Statics/Skins/Mobile/Js/app/company/caseinfo.js
@@ -0,0 +1,24 @@
+/**
+ * Created by wangzan on 2016/4/1.
+ */
+define(['zepto', 'module/company_classify', 'swiper', 'module/totop'], function (){
+   $(function (){
+      //广告
+      var $len = $('.swiper-slide').length;
+
+      var swiper = new Swiper('.swiper-container', {
+         //pagination : '.swiper-pagination',
+         // autoplay : 3000,
+         // speed:300,
+         loop : true,
+         onSlideChangeStart : function (swiper){
+            var $num = parseInt($('.swiper-slide-active').attr('data-swiper-slide-mall')) + 1;
+            $('.case_num em').html($num);
+            $('.case_num span').html($len);
+         }
+      });
+      $('.goback').click(function (){
+         history.back();
+      });
+   });
+});
