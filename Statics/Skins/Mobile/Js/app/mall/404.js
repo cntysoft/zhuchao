f59d5cab047c249065e0353908d5e72012c5d83c
wql <wql1211608804@163.com>
2016-04-15 18:29:07 +0800

    优化产品展示界面

--- Statics/Skins/Mobile/Js/app/mall/404.js
+++ Statics/Skins/Mobile/Js/app/mall/404.js
@@ -1,19 +1,8 @@
 /**
  * Created by jiayin on 2016/3/16.
  */
-define(['zepto'], function (){
+define(['zepto','module/mall_nav'], function (){
    $(function (){
-      //导航
-      $('.header_right').not('.header_right_icon_search').tap(function (){
-         var that = $('.top_nav_box');
-         if(that.hasClass('hide')){
-            $(that).removeClass('hide');
-            return false;
-         } else{
-            $(that).addClass('hide');
-            return false;
-         }
-      });
       $('.icon-sousuo').tap(function (){
          var keyword = $('.search_input').val();
          if(keyword){
